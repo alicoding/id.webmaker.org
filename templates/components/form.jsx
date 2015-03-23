de73238d1395fd1dbe02c9d6dd7100878e041cd2
Pomax <pomax@nihongoresources.com>
2015-03-23 13:19:05 -0700

    some rewrites from collab programming

--- templates/components/form.jsx
+++ templates/components/form.jsx
@@ -2,66 +2,49 @@ var React = require('react/addons');
 var ValidationMixin = require('react-validation-mixin');
 var Joi = require('joi');
 
-var Signup = React.createClass({
+var Form = React.createClass({
   propTypes: {
-    fields: React.PropTypes.array.isRequired,
-    btnTxt: React.PropTypes.string.isRequired
+    fields: React.PropTypes.array.isRequired
   },
   statics: {
       'phoneLabels': {
         'username': 'phone-label-username',
-        'password': 'phone-label-password',
-        'email': 'phone-label-email'
+        'password': 'phone-label-password'
       }
   },
-  mixins: [ValidationMixin, React.addons.LinkedStateMixin],
+  mixins: [
+    ValidationMixin,
+    React.addons.LinkedStateMixin
+  ],
   validatorTypes:  {
-    firstName: Joi.string().required().label('First Name'),
-    lastName: Joi.string().allow(null).label('Last Name'),
-    email: Joi.string().email().label('Email Address'),
-    username:  Joi.string().alphanum().min(3).max(30).required().label('Username'),
-    password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).label('Password'),
-    verifyPassword: Joi.any().valid(Joi.ref('password')).required().label('Password Confirmation')
+    username: Joi.string().alphanum().min(3).max(30).required().label('Username'),
+    password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).label('Password')
   },
   getInitialState: function() {
     return {
-      firstName: null,
-      lastName: null,
-      email: null,
       username: null,
-      password: null,
-      verifyPassword: null,
-      feedback: null
+      password: null
     };
   },
-  render: function() {
-     var that = this;
-     var fields = Object.keys(this.props.fields).map(function(key,i) {
-       var id = Object.keys(that.props.fields[i]);
-       var placeholder = that.props.fields[i][id].placeholder;
-       var type = that.props.fields[i][id].type;
-
-       return (
-            <label className={that.getIconClass(id)}>
-              <input key={id} type={type} id={id} placeholder={placeholder}
-                valueLink={that.linkState(id)} onBlur={that.handleValidation(id)}
-                className={that.getClasses(id)}
-              />
-              {that.getValidationMessages(id).map(that.renderHelpText)}
-            </label>
-       );
-     });
-     return (
-       <form role="form">
-          {fields}
-          <button type='submit' onClick={this.handleSubmit} className="btn btn-awsm">{this.props.btnTxt}</button>
-       </form>
-     );
-  },
-  renderHelpText: function(message) {
+  buildFormElement: function(key, i) {
+    var id = Object.keys(this.props.fields[i]);
+    var placeholder = this.props.fields[i][id].placeholder;
+    var type = this.props.fields[i][id].type;
+    var className = this.getIconClass(id);
     return (
-      <span className="help-block">Invalid Username or Password</span>
+     <label className={className} key={id}>
+       <input type={type}
+              id={id}
+              placeholder={placeholder}
+              valueLink={this.linkState(id)}
+              onBlur={this.handleValidation(id)}
+              className={this.getClasses(id)} />
+     </label>
     );
+  },
+  render: function() {
+     var fields = Object.keys(this.props.fields).map(this.buildFormElement);
+     return <div role="form">{fields}</div>;
   },
   getClasses: function(field) {
     return React.addons.classSet({
@@ -70,29 +53,36 @@ var Signup = React.createClass({
     });
   },
   getIconClass: function(field) {
-    var labels = this.constructor.phoneLabels;
-    return labels[field];
+    return Form.phoneLabels[field];
   },
   handleReset: function(event) {
-    event.preventDefault();
     this.clearValidations();
     this.setState(this.getInitialState());
   },
-  handleSubmit: function(event) {
-    event.preventDefault();
-    onValidate = function(error, validationErrors) {
-      if (error) {
-        this.setState({
-          feedback: 'Form is invalid do not submit'
-        });
-      } else {
-        this.setState({
-          feedback: 'Form is valid send to action creator'
-        });
+  /**
+   * "owner" components call form.processFormData on us
+   */
+  processFormData: function(callback) {
+    var self = this;
+    this.validate(function(error, data) {
+      console.log("inside Form, we see:", error, data);
+      self.onValidate(callback, error,data);
+    });
+  },
+  onValidate: function(callback, error, data) {
+    // FIXME: totally not localised yet!!
+    var feedbackText = 'Form is invalid do not submit';
+    var validText ='Form is valid send to action creator';
+    this.setState(
+      {
+        feedback: error ? feedbackText : validText
+      },
+      function postValidation() {
+        callback(error, !!error? false : JSON.parse(JSON.stringify(this.state)));
       }
-    }.bind(this);
-    this.validate(onValidate);
+    );
   }
+
 });
 
-module.exports = Signup;
+module.exports = Form;
--- templates/index.jsx
+++ templates/index.jsx
@@ -5,34 +5,40 @@ var Header = require('./components/header.jsx');
 
 // This wraps every view
 var App = React.createClass({
-  submitForm: function(e) {
-    console.log(e);
-  },
+
   render: function() {
+    // FIXME: totally not localized yet!
+    var buttonText = "Log In";
+    var fieldValues = [
+      {
+        'username': {
+          'placeholder': 'Username',
+          'type': 'text'
+        }
+      },
+      {
+        'password': {
+          'placeholder': 'Password',
+          'type': 'password'
+        }
+      }
+    ];
     return (
       <div>
-        <Header/>
-          <div className="formContainer centerDiv">
-            <Form
-              fields={
-                [
-                  { 'username': {
-                      'placeholder': 'Username',
-                      'type': 'text'
-                    }
-                  },
-                  { 'password': {
-                      'placeholder': 'Password',
-                      'type': 'password'
-                    }
-                  }
-                ]
-              }
-              btnTxt={"Log In"}
-            />
-          </div>
+        <Header />
+        <div className="formContainer centerDiv">
+          <Form ref="userform" fields={fieldValues} />
+          <button onClick={this.processFormData} className="btn btn-awsm">{buttonText}</button>
         </div>
+      </div>
     );
+  },
+  processFormData: function() {
+    var form = this.refs.userform;
+    form.processFormData(this.handleFormData);
+  },
+  handleFormData: function(error, data) {
+    console.log("inside App we see:", error, data);
   }
 });
 
